f32bed507a1b0a63657a1ce6ba6017046596b597
iecloudbird <kuroxxxshiro9@gmail.com>
2024-10-09 19:54:00 +0100

    update form and button design

--- frontend/src/app/components/SignInButton.tsx
+++ frontend/src/app/components/SignInButton.tsx
@@ -1,16 +1,18 @@
 // components/SignInButton.tsx
 "use client"; // This component should be client-side
-
 import React from "react";
+import Image from 'next/image';
 import { auth } from "../../lib/firebase"; // Adjust the path as needed
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, OAuthProvider, signInWithPopup } from "firebase/auth";
+import AppleIcon from "@/assets/images/apple-logo.png";
+import GoogleIcon from "@/assets/images/google.png";
 
-const SignInButton: React.FC = () => {
-  const provider = new GoogleAuthProvider();
+const SignInButtonGoogle: React.FC = () => {
+  const googleProvider = new GoogleAuthProvider();
 
   const signInWithGoogle = async () => {
     try {
-      const result = await signInWithPopup(auth, provider);
+      const result = await signInWithPopup(auth, googleProvider);
       const user = result.user;
       console.log("User info: ", user);
       // You can also save user info in your app state or context
@@ -22,11 +24,49 @@ const SignInButton: React.FC = () => {
   return (
     <button
       onClick={signInWithGoogle}
-      className="bg-blue-500 text-white p-2 rounded"
+      className="bg-transparent text-black px-6 py-2 rounded-xl border flex items-center justify-center space-x-1"
     >
-      Sign in with Google
+      <Image
+        src={GoogleIcon}
+        alt={"GoogleIcon"}
+        width={24}
+        height={24}
+        className="" />
+      <span className="pl-2">Sign in with Google</span>
+
     </button>
+
   );
 };
 
-export default SignInButton;
+const SignInButtonApple: React.FC = () => {
+  const appleProvider = new OAuthProvider('apple.com');
+
+  const signInWithApple = async () => {
+    try {
+      const result = await signInWithPopup(auth, appleProvider);
+      const user = result.user;
+      console.log("User info: ", user);
+      // You can also save user info in your app state or context
+    } catch (error) {
+      console.error("Error signing in: ", error);
+    }
+  };
+
+  return (
+    <button
+      onClick={signInWithApple}
+      className="bg-transparent text-black px-6 py-2 rounded-xl border flex items-center justify-center space-x-1"
+    >
+      <Image
+        src={AppleIcon}
+        alt={"AppleIcon"}
+        width={24}
+        height={24} />
+      <span className="pl-2">Sign in with Apple</span>
+    </button>
+
+  );
+};
+
+export { SignInButtonGoogle, SignInButtonApple };
--- frontend/src/app/components/auth/SignUpForm.tsx
+++ frontend/src/app/components/auth/SignUpForm.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import Image from 'next/image';
 import loginBackground from "@/assets/images/loginImage.png"
-import SignInButton from "@/app/components/SignInButton";
+import { SignInButtonGoogle, SignInButtonApple } from "@/app/components/SignInButton";
 
 
 const SignupForm = () => {
@@ -31,74 +31,83 @@ const SignupForm = () => {
         <div className="flex min-h-screen bg-white">
             <div className="flex flex-col md:flex-row w-full bg-white shadow-lg rounded-lg overflow-hidden ">
                 {/* Left Side - Form */}
-                <div className="w-full md:w-1/2 p-8 text-black">
-                    <h2 className="text-3xl font-bold mb-6">Get Started Now</h2>
-                    <form onSubmit={handleSubmit}>
-                        <div className="mb-4">
-                            <label className="block text-gray-700">Name</label>
-                            <input
-                                type="text"
-                                name="name"
-                                value={formData.name}
-                                onChange={handleChange}
-                                placeholder="Enter your name"
-                                className="w-full p-2 border rounded"
-                                required
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label className="block text-gray-700">Email address</label>
-                            <input
-                                type="email"
-                                name="email"
-                                value={formData.email}
-                                onChange={handleChange}
-                                placeholder="Enter your email"
-                                className="w-full p-2 border rounded"
-                                required
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label className="block text-gray-700">Password</label>
-                            <input
-                                type="password"
-                                name="password"
-                                value={formData.password}
-                                onChange={handleChange}
-                                placeholder="Enter your password"
-                                className="w-full p-2 border rounded"
-                                required
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label className="flex items-center">
+                <div className="w-full md:w-1/2 p-8 text-black justify-center items-center flex flex-col">
+                    <div className="w-full max-w-md text-left mx-auto">
+                        <h2 className="text-3xl font-bold mb-6">Get Started Now</h2>
+                        <form onSubmit={handleSubmit}>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 font-bold">Name</label>
                                 <input
-                                    type="checkbox"
-                                    name="agreeToTerms"
-                                    checked={formData.agreeToTerms}
+                                    type="text"
+                                    name="name"
+                                    value={formData.name}
                                     onChange={handleChange}
-                                    className="mr-2"
+                                    placeholder="Enter your name"
+                                    className="w-full p-2 border rounded"
                                     required
                                 />
-                                I agree to the {" "}<a href="#" className="text-blue-600 underline">terms & policy</a>
-                            </label>
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 font-bold">Email address</label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                    placeholder="Enter your email"
+                                    className="w-full p-2 border rounded"
+                                    required
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 font-bold">Password</label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    value={formData.password}
+                                    onChange={handleChange}
+                                    placeholder="Enter your password"
+                                    className="w-full p-2 border rounded"
+                                    required
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="flex items-center">
+                                    <input
+                                        type="checkbox"
+                                        name="agreeToTerms"
+                                        checked={formData.agreeToTerms}
+                                        onChange={handleChange}
+                                        className="mr-2"
+                                        required
+                                    />
+                                    I agree to the&nbsp;<a href="#" className="text-blue-600 underline"> {"  "}terms & policy</a>
+                                </label>
+                            </div>
+                            <button
+                                type="submit"
+                                className="w-full bg-orange-500 text-white font-bold p-2 rounded-lg hover:bg-orange-600 transition duration-300"
+                            >
+                                Signup
+                            </button>
+                        </form>
+                        <div className="flex items-center justify-center my-4 w-full">
+                            <hr className="w-full text-gray-300" />
+                            <span className="mx-4 text-gray-500">Or</span>
+                            <hr className="w-full border-gray-300" />
                         </div>
-                        <button
-                            type="submit"
-                            className="w-full bg-orange-500 text-white font-bold p-2 rounded-lg hover:bg-orange-600 transition duration-300"
-                        >
-                            Signup
-                        </button>
-                    </form>
 
-                    <div className="text-center my-4">Or</div>
-                    <div className="flex justify-between">
-                        <SignInButton />
-                        <button className="w-1/2 bg-gray-100 p-2 rounded-lg flex justify-center items-center ml-2">
-                            <Image src="/apple-icon.png" alt="Apple" width={24} height={24} className="mr-2" />
-                            Sign in with Apple
-                        </button>
+                        <div className="flex justify-evenly">
+                            <SignInButtonGoogle />
+                            <SignInButtonApple />
+                            {/* <button className="w-1/2 bg-gray-100 p-2 rounded-lg flex justify-center items-center ml-2">
+                                <Image src="/apple-icon.png" alt="Apple" width={24} height={24} className="mr-2" />
+                                Sign in with Apple
+                            </button> */}
+                        </div>
                     </div>
+
+
 
                     <p className="text-center mt-4">
                         Have an account? <a href="/signin" className="text-blue-600 underline">Sign In</a>
@@ -106,16 +115,16 @@ const SignupForm = () => {
                 </div>
 
                 {/* Right Side - Image */}
-                <div className="relative w-full md:w-1/2 h-64 md:h-auto">
+                <div className="relative w-full md:w-1/2 h-64 md:h-screen">
                     <Image
                         src={loginBackground}
                         alt="Login Background"
                         fill
-                        className="object-cover"
+                        className="rounded-3xl"
                     />
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
