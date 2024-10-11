38c99dc8e7a2fcd758ba53aa7b18efaacdec8783
ManavYagnik <yagnik.c@ahduni.edu.in>
2024-10-11 16:10:11 +0530

    logut button added for the testing purpose

--- frontend/src/app/components/UserProfile.tsx
+++ frontend/src/app/components/UserProfile.tsx
@@ -1,21 +1,31 @@
 // components/UserProfile.tsx
 "use client"; // This component should be client-side
 import React from "react";
+import { getAuth, signOut } from "firebase/auth";
 import { useAuth } from "../../context/AuthContext";
 import Image from 'next/image';
 import SignInPage from "@/app/auth/page"
 
 const UserProfile: React.FC = () => {
   const { user } = useAuth();
-
+  // console.log(user?.displayName);
   if (!user) {
     return <div>
       <SignInPage />
     </div>;
   }
-
+  const handleLogout = async () => {
+    const auth = getAuth();
+    try {
+      await signOut(auth);
+      console.log('User successfully logged out');
+      // You can also redirect the user to a login page or another route if needed
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  };
   return (
-    <div>
+    <div className="h-full bg-red-800">
       {user.photoURL && (
         <Image
           src={user.photoURL}
@@ -25,6 +35,7 @@ const UserProfile: React.FC = () => {
         />
       )}
       <p>{user.displayName || 'Anonymous User'}</p>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 };
